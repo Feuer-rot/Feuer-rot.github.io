8855f46565782caa38a4bc3ef918ab0a168a6713
Feuer-rot <feuer_rot@outlook.de>
2025-11-03 22:16:35 +0100

    three.js funktioniert
    hell yeah

--- js/3dmodels.js
+++ js/3dmodels.js
@@ -1,10 +1,12 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
+    antialias: true, alpha: true,
     canvas: document.querySelector('.container3D')
 });
 
@@ -12,22 +14,37 @@ const container = document.querySelector('.container3D');
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(container.clientWidth, container.clientHeight);
 
-camera.position.setZ(30);
+camera.position.setZ(10);
 camera.lookAt(0, 0, 0);
 
-const spotLight = new THREE.SpotLight(0xffffff, 3, 100, .2, 0.5);
-spotLight.position.set(0, 25, 0);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(spotLight, ambientLight);
+const pointLight1 = new THREE.PointLight(0xffffff, 100);
+pointLight1.position.set(5,5,5);
+const pointLight2 = new THREE.PointLight(0xffffff, 50);
+pointLight2.position.set(-5,5,5);
+const pointLight3 = new THREE.PointLight(0xffffff, 50);
+pointLight3.position.set(-5, 0,-5);
+scene.add(pointLight1, pointLight2, pointLight3);
 
-const lightHelper = new THREE.PointLightHelper(spotLight)
-scene.add(lightHelper)
+// const lightHelper = new THREE.PointLightHelper(pointLight1)
+// scene.add(lightHelper)
+
+// const lightHelper1 = new THREE.PointLightHelper(pointLight2)
+// scene.add(lightHelper1)
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const loader = new GLTFLoader();
 loader.load(
-  'models/taube/scene.gltf',
+  'models/taube/taube.glb',
   (gltf) => {
+    const model = gltf.scene;
+    const box = new THREE.Box3().setFromObject(model);
+    const center = new THREE.Vector3();
+    box.getCenter(center);
+    model.position.sub(center); // move model so its center is at (0, 0, 0)
     scene.add(gltf.scene);
   },
   undefined,
@@ -35,6 +52,7 @@ loader.load(
     console.error('Fehler beim Laden des Modells:', error);
   }
 );
+
 
 function animate() {
     requestAnimationFrame(animate);
